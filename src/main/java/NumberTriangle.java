a3da03d231a85398b1f9ab429f810c56bbfbb1d4
huangyaohui <yaohui.huang@utoronto.student.ca>
2025-10-02 11:23:33 -0400

    Add static method

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 
 /**
  * This is the provided NumberTriangle class to be used in this coding task.
@@ -110,7 +111,7 @@ public class NumberTriangle {
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
 
-        // TODO define any variables that you want to use to store things
+        java.util.List<java.util.List<Integer>> list = new java.util.ArrayList<>();
 
         // will need to return the top of the NumberTriangle,
         // so might want a variable for that.
@@ -118,11 +119,13 @@ public class NumberTriangle {
 
         String line = br.readLine();
         while (line != null) {
-
-            // remove when done; this line is included so running starter code prints the contents of the file
-            System.out.println(line);
-
-            // TODO process the line
+            line = line.trim();
+            if (!line.isEmpty()) {
+                String[] parts = line.split("\\s+");
+                java.util.List<Integer> row = new java.util.ArrayList<>(parts.length);
+                for (String part : parts) row.add(Integer.parseInt(part));
+                list.add(row);
+            }
 
             //read the next line
             line = br.readLine();
